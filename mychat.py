994a1f54c6a8c9a183f04461dbd78c621f243008
Joshe <joshe21br>
2021-05-28 10:26:01 -0300

    Reorganizando o código

--- mychat.py
+++ mychat.py
@@ -1,17 +1,24 @@
 #!-*-coding: utf-8 -*-
-
+#
+# josuesouzadasilva@gmail.com
+# joshebr18@protonmail.com
+# Linkdin: Josué Souza da Silva
+# Gitlab: https://gitlab.com/joshe_sotero
+# Pequenos projetos pessoais: http://joshe.epizy.com/
+# https://django1-js.herokuapp.com
 """
-Criação do chatbot com NLTK
+Criação do chatbot com NLTK -->> Nivel Básico
 ---------------------------
-# Chat: a lógica usada pelo chatbot
-# reflections: dicionários com entradas e suas respctivas saidas
+# Teste realizado na propria máquina utilizando o postman
+# Resultado: 100 funcionando
 """
 
-# importação dos modulos
+#------------------------------------
+# Etapa 1: Importação das bibliotecas
+#------------------------------------
 import os
 import requests
 from flask import Flask, request, jsonify 
-
 import bs4 as bs
 import urllib.request
 import re
@@ -26,33 +33,29 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 nltk.download('punkt')
 
-#----==----------------
-#   Corpo de codigos 
-#----------------------
+#----------------------------------------------------------
+# Etapa 2: Carregamento e pré-pocessamento da base de dados
+#----------------------------------------------------------
 print('\nTodo o conteúdo do html\n')
-dados = urllib.request.urlopen('https://pt.wikipedia.org/wiki/Intelig%C3%AAncia_artificial')
+dados = urllib.request.urlopen('adicionar a url para baixar as iformações')
 dados = dados.read()
 print(dados)
-#--------------------
+
 print('\nSomente o html de forma mais organizado\n')
 dados_html = bs.BeautifulSoup(dados, 'lxml')
 print(dados_html)
-#--------------------
+
 print('\nApenas os parágrafos\n')
 paragrafos = dados_html.find_all('p')
 print(paragrafos)
-#--------------------
 print('\n')
-#--------------------
 
 print('Busca por parágrafos\n')
 print(paragrafos[0])
 print('\n')
-#---------------------
 
 print('Mostrar sem as tags\n')
 print(paragrafos[0].text)
-#--------------------
 
 print('Mostrar todos os textos sem as tags\n')
 
@@ -61,18 +64,14 @@ for p in paragrafos:
     conteudo += p.text
 
 print(conteudo)
-
 print('')
-#-------------------
 
 print('Pré-processamento\n')
 print('Transformando todas as letras em minúsculas\n')
 
 conteudo = conteudo.lower()
-
 print(conteudo)
 print('')
-#-------------------
 
 print('Separando por frases\n')
 lista_sentencas = nltk.sent_tokenize(conteudo)
@@ -80,45 +79,36 @@ lista_sentencas = nltk.sent_tokenize(conteudo)
 print(lista_sentencas)
 print('')
 
-#-------------------
-
 print('Quantidade de frases\n')
 print(len(lista_sentencas))
 print('')
 
-#-------------------
 print('Tipo da variável\n')
 print(type(lista_sentencas))
-#------------------
     
 print('Selecionar paragrafo\n')
 print(lista_sentencas[0])
 print('')
-#------------------
 
 pln = spacy.load('pt')
 print(pln)
-#------------------
 
 print('As stop_words\n')
 stop_words = spacy.lang.pt.stop_words.STOP_WORDS
 print(stop_words)
 print('')
-#-----------------
 
 print(type(stop_words))
 print(len(stop_words))
 print('')
-#--------------------
 
 print(string.punctuation)
 print('')
-#--------------------
 
 print('Remoção de URLs\n')
 
 def preprocessamento(texto):
-    #URLs
+    # Remoção das URLs
     texto = re.sub("https?://[A-Za-z0-9./]+", ' ', texto)
     # Espaço em branco
     texto = re.sub(r' +', ' ', texto ) 
@@ -138,15 +128,12 @@ def preprocessamento(texto):
     return lista
 
 
-texto_teste = 'https://www.iaexpert.com.br ' + lista_sentencas[0]
+texto_teste = ' Adcionar a url para extração e processamento  ' + lista_sentencas[0]
 print(texto_teste)
 print('')
-#-------------------
 
 resultado = preprocessamento(texto_teste)
 print(resultado)
-
-#------------------
 
 lista_processada = []
 for i in range(len(lista_sentencas)):
@@ -159,9 +146,9 @@ for _ in range(5):
     print('-------')
 
 
-#----------------------
-# Texto chatbot
-
+#------------------------------------
+# Etapa 3:  Mensagens de boas-vindas
+#------------------------------------
 saudacoes = ('hey', 'ola', 'oi', 'opa', 'eae')
 resposta = ('hey', 'ola', 'oi', 'opa', 'Como vai!', 'Oi vc esta bem?')
 
@@ -175,83 +162,63 @@ print('Olá, tudo bem?')
 
 resp = str(input('>>: '))
 print(responder_saudacao(resp))
-
 print('')
-
-#--------------
 
 frases_teste = lista_processada[:3]
 print(frases_teste)
 print('')
-#---------------
 
 frases_teste.append(frases_teste[0])
 print(frases_teste)
 print('')
+
 #---------------
 # vetores
 #--------------
 
 vectores_palavras = TfidfVectorizer()
 palavras_vectorizadas = vectores_palavras.fit_transform(frases_teste)
-
 print(type(palavras_vectorizadas))
-
 print('')
-
 print(palavras_vectorizadas)
 print('')
-
 print(vectores_palavras.get_feature_names())
-print()
-#-------------------
-
-
+print('')
 print(len(vectores_palavras.get_feature_names()))
 print('')
-
-
 print(vectores_palavras.vocabulary_)
-print()
-
+print('')
 print(vectores_palavras.idf_)
-print()
-
+print('')
 print(palavras_vectorizadas.todense())
-print()
-
+print('')
 print(palavras_vectorizadas.todense().shape)
-print()
+print('')
+
 #----------------------
-#Cossine similaridades
+#Cosine similaridades
 #---------------------
 print(cosine_similarity(palavras_vectorizadas[0], palavras_vectorizadas[1]))
-print()
-
+print('')
 print(cosine_similarity(palavras_vectorizadas[0], palavras_vectorizadas[3]))
-print()
-
+print('')
 print(cosine_similarity(palavras_vectorizadas[0], palavras_vectorizadas))
-print()
+print('')
 
 similaridade = cosine_similarity(palavras_vectorizadas[0], palavras_vectorizadas)
 print(similaridade)
 print(similaridade.argsort())
-print()
-#-----------------
+print('')
 
 i = similaridade.argsort()[0][-2]
 print(i)
-print()
-
+print('')
 i = i.flatten()
-
 print(i)
-#----------------------------------
 
-
-# Resposta do chatbot
-
+#---------------------------------------------
+# Etapa 4: Função para as Resposta do chatbot
+#---------------------------------------------
 def responder(texto_usuario):
     resposta_chatbot = ''
     lista_processada.append(texto_usuario)
@@ -273,12 +240,14 @@ def responder(texto_usuario):
         resposta_chatbot = resposta_chatbot + lista_sentencas[indice_sentenca]
         return resposta_chatbot
 
+#------------------------------
 # Etapa 5 criação da API Flask
-
+#------------------------------
 app = Flask(__name__)
 
-# Função que retorna as respostas
-
+#------------------------------------------
+# Etapa 6: Função que retorna as respostas
+#------------------------------------------
 @app.route("/<string:txt>", methods=["POST"])
 
 def conversar(txt):
@@ -291,9 +260,9 @@ def conversar(txt):
         resposta = responder(preprocessamento(texto_usuario))
         lista_processada.remove(preprocessamento(texto_usuario))
     return jsonify({"texto_responder": resposta})
-    
 
+#-------------------------------    
+# Etapa 7:  Iniciar a aplicação
+#-------------------------------
 app.run(port=5000, debug=False)
-
-
 
